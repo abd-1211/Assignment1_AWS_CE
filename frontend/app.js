74e869c56c8156997fb05ec565a408291d0034f8
Abdullah Waheed <abdullah5678910@gmail.com>
2026-03-25 15:23:49 +0500

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,15 @@
-fetch("http://localhost:3000/events")
-.then(res => res.json())
-.then(data => {
-    const container = document.getElementById("events");
-
-    data.forEach(event => {
-        const div = document.createElement("div");
-        div.innerHTML = `
-            <h3>${event.title}</h3>
-            <p>${event.date} - ${event.venue}</p>
-            <img src="${event.image}" width="200"/>
-        `;
-        container.appendChild(div);
-    });
-});
+fetch('http://Eventmanager-ALB-709254883.ap-south-1.elb.amazonaws.com/events')
+  .then(res => res.json())
+  .then(data => {
+      const container = document.getElementById('events');
+      data.forEach(event => {
+          const div = document.createElement('div');
+          // include image from S3
+          div.innerHTML = `
+              <h3>${event.title}</h3>
+              <p>${event.date} - ${event.venue}</p>
+              ${event.image ? `<img src="${event.image}" alt="Event Poster" width="200">` : ""}
+          `;
+          container.appendChild(div);
+      });
+  });
